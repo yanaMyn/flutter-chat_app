8118d9c2d9be39e487f54809a6da69dba94f6de6
Yana Mulyana <yanamyn15@gmail.com>
2023-12-02 21:23:02 +0700

    show messages

--- lib/widgets/chat_messages.dart
+++ lib/widgets/chat_messages.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessages extends StatelessWidget {
@@ -5,6 +6,30 @@ class ChatMessages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("No message"));
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection('chat')
+          .orderBy('createdAt', descending: false)
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator();
+        }
+
+        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+          return const Center(
+            child: Text("No message.."),
+          );
+        }
+
+        final items = snapshot.data!.docs;
+        return ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            return Text(items[index].data()['text']);
+          },
+        );
+      },
+    );
   }
 }
