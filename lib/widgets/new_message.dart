2ee61be2fdd21409863ab9b80ec879a3c224b232
Yana Mulyana <yanamyn15@gmail.com>
2023-12-02 21:07:02 +0700

    send message

--- lib/widgets/new_message.dart
+++ lib/widgets/new_message.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class NewMessage extends StatefulWidget {
@@ -18,14 +20,29 @@ class _NewMessage extends State<NewMessage> {
     _messageController.dispose();
   }
 
-  void _submitMessage() {
+  void _submitMessage() async {
     final enteredMessage = _messageController.text;
 
     if (enteredMessage.trim().isEmpty) {
       return;
     }
 
+    FocusScope.of(context).unfocus();
     _messageController.clear();
+
+    final user = FirebaseAuth.instance.currentUser!;
+    final userData = await FirebaseFirestore.instance
+      .collection("users")
+      .doc(user.uid)
+      .get();
+
+    FirebaseFirestore.instance.collection('chat').add({
+      'text': enteredMessage,
+      'createdAt': Timestamp.now(),
+      'userId': user.uid,
+      'username': userData.data()!['username'],
+      'userImage': userData.data()!['imageUrl'],
+    });
   }
 
   @override
@@ -33,12 +50,13 @@ class _NewMessage extends State<NewMessage> {
     return Padding(
       padding: const EdgeInsets.only(left: 15, right: 1, bottom: 34),
       child: Row(children: [
-        const Expanded(
+        Expanded(
           child: TextField(
+            controller: _messageController,
             textCapitalization: TextCapitalization.sentences,
             autocorrect: true,
             enableSuggestions: true,
-            decoration: InputDecoration(labelText: "Send a message.."),
+            decoration: const InputDecoration(labelText: "Send a message.."),
           ),
         ),
         IconButton(
